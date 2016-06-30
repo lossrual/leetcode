d365cf2f1e862d895256c69a960c084ff134b96b
sun <lossrual@163.com>
2016-06-30 14:07:11 +0800

    [94. Binary Tree Inorder Traversal][Accepted]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.cpp
+++ 94-Binary-Tree-Inorder-Traversal/solution.cpp
@@ -11,28 +11,21 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-        TreeNode* tmp = root, *pre;
+        //TreeNode* tmp = root, *pre;
         if(!root) return res;
-        while(tmp){
-            if(!tmp->left){
-                res.push_back(tmp->val);
-                tmp = tmp->right;
-            }
-            else{
-                pre = tmp->left;
-                while(pre->right && pre->right != tmp)
-                    pre = pre->right;
-                if(!pre->right){
-                    pre->right = tmp;
-                    tmp = tmp->left;
-                }
-                else{
-                    res.push_back(tmp->val);
-                    pre->right = nullptr;
-                    tmp = tmp->right;
-                }
+        stack<TreeNode*> s;
+        TreeNode* cur = root;
+        while(!s.empty() || cur){
+            if(cur){
+                s.push(cur);
+                cur = cur->left;
+            }else{
+                TreeNode* p = s.top();
+                res.push_back(p->val);
+                s.pop();
+                cur = p->right;
             }
         }
-        
+        return res;
     }
 };
