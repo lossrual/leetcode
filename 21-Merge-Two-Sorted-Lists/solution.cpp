99af15ffe944301db311403bd70cb395b8d04cf2
sun <lossrual@163.com>
2016-07-05 19:01:07 +0800

    [21. Merge Two Sorted Lists][Accepted]committed by LeetCode Extension

--- 21-Merge-Two-Sorted-Lists/solution.cpp
+++ 21-Merge-Two-Sorted-Lists/solution.cpp
@@ -19,4 +19,19 @@ public:
             return l2;
         }
     }
+    /*普通lists的合并
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = l1;
+        while(l2){
+            ListNode* cur = l1->next;
+            l1 = l1->next = l2;
+            //l1 = l1->next;
+            l2 = cur;
+        }
+        return dummy->next;
+    }
+    */
 };
