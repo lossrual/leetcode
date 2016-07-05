1177f1944472882ddccaf5cf4f8e080b9203d197
sun <lossrual@163.com>
2016-07-05 12:20:29 +0800

    [2. Add Two Numbers][Accepted]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.cpp
+++ 2-Add-Two-Numbers/solution.cpp
@@ -9,16 +9,18 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode prehead(-1); ListNode *p = &prehead;
-        int extra = 0;
+        ListNode* dummy = new ListNode(0);
+        ListNode* p = dummy;
+        int extra = 0, sum = 0;
+        //extra 是进位
         while(l1 || l2 || extra){
-            int sum = (l1 ? l1 -> val : 0) + (l2 ? l2 -> val : 0) + extra;
+            sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
             extra = sum / 10;
-            p -> next = new ListNode(sum % 10);
-            p = p -> next;
-            l1 = l1 ? l1 -> next : l1;
-            l2 = l2 ? l2 -> next : l2;
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+            l1 = l1 ? l1->next: l1;
+            l2 = l2 ? l2->next: l2;
         }
-        return prehead.next;
+        return dummy->next;
     }
 };
