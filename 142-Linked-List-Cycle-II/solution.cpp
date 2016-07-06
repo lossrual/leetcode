a9fb0213e1d5899a65ada6089f7e143948b1db20
sun <lossrual@163.com>
2016-07-06 16:36:24 +0800

    [142. Linked List Cycle II][Accepted]committed by LeetCode Extension

--- 142-Linked-List-Cycle-II/solution.cpp
+++ 142-Linked-List-Cycle-II/solution.cpp
@@ -8,10 +8,27 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
+    ListNode *detectCycle1(ListNode *head) {
         if(!head) return head;
         ListNode* slow = head, *fast = head;
         while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                ListNode* slow2 = head;
+                while(slow2 != slow){
+                    slow = slow->next;
+                    slow2 = slow2->next;
+                }
+                return slow2;
+            }
+        }
+        return nullptr;
+    }
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return head;
+        ListNode* slow = head, *fast = head;
+        while(fast&& fast->next){
             slow = slow->next;
             fast = fast->next->next;
             if(slow == fast){
