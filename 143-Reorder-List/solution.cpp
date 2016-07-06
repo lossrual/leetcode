ac0912bff74e8369aeed4dce65859ec3028cb918
sun <lossrual@163.com>
2016-07-06 16:15:26 +0800

    [143. Reorder List][Accepted]committed by LeetCode Extension

--- 143-Reorder-List/solution.cpp
+++ 143-Reorder-List/solution.cpp
@@ -8,10 +8,9 @@
  */
 class Solution {
 public:
-    void reorderList(ListNode* head) {
-        if(!head || !head->next) return;
-        ListNode* slow = head;
-        ListNode* fast = head;
+    void reorderList1(ListNode* head) {
+        if(!head) return;
+        ListNode* slow = head, *fast = head;
         while(fast->next && fast->next->next){
             slow = slow->next;
             fast = fast->next->next;
@@ -26,11 +25,33 @@ public:
             head2 = nxt;
         }
         head2 = pre;
-        while(head2){
+        while(head){
             ListNode* cur = head->next;
             head = head->next = head2;
             head2 = cur;
         }
-        //return head;
+    }
+    void reorderList(ListNode* head) {
+        if(!head) return;
+        ListNode* slow = head, *fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* head2 = slow;
+        //slow->next = nullptr;
+        ListNode* pre = nullptr;
+        while(head2){
+            ListNode* nxt = head2->next;
+            head2->next = pre;
+            pre = head2;
+            head2 = nxt;
+        }
+        head2 = pre;
+        while(head){
+            ListNode* cur = head->next;
+            head = head->next = head2;
+            head2 = cur;
+        }
     }
 };
