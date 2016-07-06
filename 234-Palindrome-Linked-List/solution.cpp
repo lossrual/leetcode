8d87ba7a17c2070e7107127de3d056005ce716e3
sun <lossrual@163.com>
2016-07-06 16:13:24 +0800

    [234. Palindrome Linked List][Accepted]committed by LeetCode Extension

--- 234-Palindrome-Linked-List/solution.cpp
+++ 234-Palindrome-Linked-List/solution.cpp
@@ -8,7 +8,7 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
+    bool isPalindrome1(ListNode* head) {
         if(!head || !head->next) return true;
         ListNode* slow = head, *fast = head;
         while(fast->next && fast->next->next){
@@ -17,6 +17,25 @@ public:
         }
         ListNode* mid = slow->next;
         slow->next = nullptr;
+        ListNode* newhead = reverse(mid);
+        while(head && newhead){
+            if(head->val == newhead->val){
+                head = head->next;
+                newhead = newhead->next;
+            }else
+                return false;
+        }
+        return true;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next) return true;
+        ListNode* slow = head, *fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* mid = slow;
+        //slow->next = nullptr;
         ListNode* newhead = reverse(mid);
         while(head && newhead){
             if(head->val == newhead->val){
