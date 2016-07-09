a20839f13a9d645152e63f7302d4a53404c278f5
sun <lossrual@163.com>
2016-07-09 20:42:05 +0800

    [297. Serialize and Deserialize Binary Tree][Compile Error]committed by LeetCode Extension

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.cpp
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.cpp
@@ -12,37 +12,32 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        ostringstream out;
-        serialize(root, out);
-        return out.str();
+        if(!root) return '#';
+        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right); 
         
     }
     
     TreeNode* deserialize(string data) {
-        istringstream in(data);
-        return deserialize(in);
+        return mydeserialize(data);
     }
 private:
-    void serialize(TreeNode* root, ostringstream& out){
-        if(root){
-            out << root->val << ' ';
-            serialize(root->left, out);
-            serialize(root->right, out);
+    TreeNode* mydeserialize(string& data){
+        if(data[0] == "#"){
+            if(data.size() > 1) data = data.substr(2);
+            else return nullptr;
         }else{
-            out << "# ";//注意有空格
+            auto node = new TreeNode(helper(data));
+            node->left = mydeserialize(data);
+            node->right = mydeserialize(data);
+            return node;
         }
     }
-
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(istringstream& in){
-        string val;
-        in >> val;
-        if(val == "#") return nullptr;
-        TreeNode* root = new TreeNode(stoi(val));
-        root->left = deserialize(in);
-        root->right = deserialize(in);
-        return root;
-    } 
+    int helper(string& data){
+        int pos = data.find(',');
+        int val = stoi(data.substr(0, pos));
+        data = data.substr(pos + 1);
+        return val;
+    }
 };
 
 // Your Codec object will be instantiated and called as such:
