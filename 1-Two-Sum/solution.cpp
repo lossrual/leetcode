2b5118a03130eaa6f1afd960fa3eecceff68b786
sun <lossrual@163.com>
2016-07-10 21:18:50 +0800

    [1. Two Sum][Accepted]committed by LeetCode Extension

--- 1-Two-Sum/solution.cpp
+++ 1-Two-Sum/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int>mp;
         vector<int> res;
+        if(nums.size() == 0) return res;
+        unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++){
             int other = target - nums[i];
             if(mp.find(other) != mp.end()){
                 res.push_back(mp[other]);
                 res.push_back(i);
-                return res;
             }
-        mp[nums[i]] = i;    
+            mp[nums[i]] = i;
         }
+        return res;
     }
 };
