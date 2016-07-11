224eada51f77a51ea7ce8b52f468e192566e76a5
sun <lossrual@163.com>
2016-07-11 21:40:44 +0800

    [349. Intersection of Two Arrays][Accepted]committed by LeetCode Extension

--- 349-Intersection-of-Two-Arrays/solution.cpp
+++ 349-Intersection-of-Two-Arrays/solution.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
-//set
-    vector<int> intersection1(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
-        unordered_set<int> set_(nums1.begin(), nums1.end());
-        for(auto a: nums2){
-            if(set_.count(a)){
-                res.push_back(a);
-                set_.erase(a);
-            }
-        }
-        return res;
-    }
-    //map time:O(n + m) space:O(m)
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> res;
-        unordered_map<int, int> map_;
-        for(int i = 0; i < nums1.size(); i++)
-            map_[nums1[i]]++;
-        for(int i = 0; i < nums2.size(); i++){
-            if(map_.find(nums2[i]) != map_.end() && map_[nums2[i]] > 0){
-                res.push_back(nums2[i]);
-                map_[nums2[i]] = 0;
+        if(nums1.size() == 0 || nums2.size() == 0) return res;
+        unordered_set<int> st(nums1.begin(), nums1.end());
+        for(auto& a: nums2){
+            if(st.count(a)){
+                res.push_back(a);
+                st.erase(a);
             }
         }
         return res;
