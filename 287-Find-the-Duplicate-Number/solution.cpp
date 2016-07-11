71d25faa3db30e97b6aeb008491f6cfaa1b42095
sun <lossrual@163.com>
2016-07-11 20:33:11 +0800

    [287. Find the Duplicate Number][Accepted]committed by LeetCode Extension

--- 287-Find-the-Duplicate-Number/solution.cpp
+++ 287-Find-the-Duplicate-Number/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate1(vector<int>& nums) {
         if(nums.size() == 0) return 0;
         unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++){
@@ -12,5 +12,24 @@ public:
             }
         }
         return 0;
+    }
+    //O(nlogn)
+    int findDuplicate(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int begin = 1, end = nums.size() - 1;
+        while(begin < end){
+            int mid = (begin + end) / 2;
+            int count = 0;
+            for(int num: nums){
+                if(num <= mid)
+                    ++count;
+            }
+            if(count > mid){
+                end = mid;
+            }else{
+                begin = mid + 1;
+            }
+        }
+        return begin;
     }
 };
