de16b55e872e02e5c94f7f533a362746eb8dc617
sun <lossrual@163.com>
2016-07-11 13:41:11 +0800

    [42. Trapping Rain Water][Accepted]committed by LeetCode Extension

--- 42-Trapping-Rain-Water/solution.cpp
+++ 42-Trapping-Rain-Water/solution.cpp
@@ -1,29 +1,10 @@
 class Solution {
 public:
-    int trap1(vector<int>& height) {
-        int maxn = 0;
-        int leftmax = 0, rightmax = 0;
-        int i = 0, j = height.size() - 1;
-        while(i <= j){
-            leftmax = max(leftmax, height[i]);
-            rightmax = max(rightmax, height[j]);
-            if(leftmax < rightmax){
-                maxn += (leftmax - height[i]);
-                ++i;
-            }else{
-                maxn += (rightmax - height[j]);
-                --j;
-            }
-        }
-        return maxn;
-    }
     int trap(vector<int>& height) {
         int maxn = 0;
-        //int leftmax = 0, rightmax = 0;
         int i = 0, j = height.size() - 1;
         while(i < j){
             int minn = min(height[i], height[j]);
-            std::cout << i << ' ' << j << std::endl;
             if(height[i] == minn){
                 ++i;
                 while(i < j && height[i] < minn){
@@ -34,7 +15,7 @@ public:
                 --j;
                 while(i < j && height[j] < minn){
                     maxn += (minn - height[j]);
-                    --j;
+                    --j;  
                 }
             }
         }
