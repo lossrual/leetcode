67ce34ac867fe0e1f4a6c3192db97a10d13fb993
sun <lossrual@163.com>
2016-07-12 11:46:31 +0800

    [56. Merge Intervals][Accepted]committed by LeetCode Extension

--- 56-Merge-Intervals/solution.cpp
+++ 56-Merge-Intervals/solution.cpp
@@ -14,9 +14,13 @@ public:
         if(intervals.empty()) return vector<Interval>{};
         sort(intervals.begin(), intervals.end(), [](Interval a, Interval b){return a.start < b.start;});
         res.push_back(intervals[0]);
-        for(int i = 1; i < intervals.size(); i++){
-            if(res.back().end < intervals[i].start) res.push_back(intervals[i]);
-            else res.back().end = max(res.back().end, intervals[i].end);
+        //std::cout << 1 << std::endl;
+        intervals.erase(intervals.begin());
+        for(auto c : intervals){
+            if(res.back().end < c.start) res.push_back(c);
+            else{
+                res.back().end = max(res.back().end, c.end);
+            }
         }
         return res;
     }
