fff5890237bc97a57f2ac63fb9072a50126a920c
sun <lossrual@163.com>
2016-07-12 20:25:02 +0800

    [240. Search a 2D Matrix II][Accepted]committed by LeetCode Extension

--- 240-Search-a-2D-Matrix-II/solution.cpp
+++ 240-Search-a-2D-Matrix-II/solution.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
-        int row = 0, col = n - 1;
-        while(row <= m - 1 && col >= 0){
-            if(target > matrix[row][col]) row++;
-            else if(target < matrix[row][col]) col--;
+        int a = 0, b = n - 1;
+        while(a <= m - 1 && b >= 0){
+            if(target < matrix[a][b]) b--;
+            else if(target > matrix[a][b]) a++;
             else{
                 return true;
             }
