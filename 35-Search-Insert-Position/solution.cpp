97a0dd6f2eb06f7001ef70edfe6d5dfa6d55d359
sun <lossrual@163.com>
2016-07-12 10:38:30 +0800

    [35. Search Insert Position][Accepted]committed by LeetCode Extension

---
+++ 35-Search-Insert-Position/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(nums.size() == 0) return 0;
+        int low = 0, high = nums.size();
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] > target){
+                high = mid;
+            }else{
+                low = mid + 1;
+                //std::cout << low << std::endl;
+            }
+        }
+        return low;
+    }
+};
