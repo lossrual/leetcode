82084b40b54a694a42efa7e36ae0ca18c2b880c6
sun <lossrual@163.com>
2016-07-12 21:49:52 +0800

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.cpp
+++ 75-Sort-Colors/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 //two pass
-    void sortColors(vector<int>& nums) {
+    void sortColors1(vector<int>& nums) {
         int red = 0, white = 0, blue = 0;
         for(auto c: nums){
             if(c == 0) ++red;
@@ -20,6 +20,22 @@ public:
                 continue;
             }
             nums[i] = 2;
+        }
+    }
+    //one pass
+    void sortColors(vector<int>& nums) {
+        int two = 0, one = 0, zero = 0;
+        for(auto c: nums){
+            if(c == 2){
+                nums[two++] = 2;
+            }else if(c == 1){
+                nums[two++] = 2;
+                nums[one++] = 1;
+            }else{
+                nums[two++] = 2;
+                nums[one++] = 1;
+                nums[zero++] = 0;
+            }
         }
     }
 };
