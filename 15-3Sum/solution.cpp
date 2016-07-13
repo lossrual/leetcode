5109aa133c11686e59d05866a1824022fcbee439
sun <lossrual@163.com>
2016-07-13 22:45:33 +0800

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.cpp
+++ 15-3Sum/solution.cpp
@@ -3,27 +3,24 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        if(nums.size() == 0) return res;
-        int first = 0, last = 0, target = 0, sum;
-        //unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++){
-            target = -nums[i];
-            first = i + 1, last = nums.size() - 1;
-            while(first < last){
-                sum = nums[first] + nums[last];
-                if(sum < target) ++first;
-                else if(sum > target) --last;
+            int sum = -nums[i];
+            int low = i + 1, high = nums.size() - 1;
+            while(low < high){
+                int cur = nums[low] + nums[high];
+                if(cur < sum) ++low;
+                else if(cur > sum) --high;
                 else{
                     vector<int> triplet(3, 0);
                     triplet[0] = nums[i];
-                    triplet[1] = nums[first];
-                    triplet[2] = nums[last];
+                    triplet[1] = nums[low];
+                    triplet[2] = nums[high];
                     res.push_back(triplet);
-                    while(first < last && triplet[1] == nums[first]) ++first;
-                    while(first < last && triplet[2] == nums[last]) --last;
+                    while(low < high && triplet[1] == nums[low]) ++low;
+                    while(low < high && triplet[2] == nums[high]) --high;
                 }
             }
-            while(nums[i] == nums[i + 1] && i < nums.size() - 1) ++i;
+            while(i < nums.size() - 1 && nums[i] == nums[i + 1]) ++i;
         }
         return res;
     }
