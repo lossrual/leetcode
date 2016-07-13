c60ec8b5be7ac9780aaca18651f7fa9dfad6f9dd
sun <lossrual@163.com>
2016-07-13 22:35:35 +0800

    [16. 3Sum Closest][Accepted]committed by LeetCode Extension

--- 16-3Sum-Closest/solution.cpp
+++ 16-3Sum-Closest/solution.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        int sum = nums[0] + nums[1] + nums[2];
         sort(nums.begin(), nums.end());
-        int res = nums[0] + nums[1] + nums[2];
-        for(int i = 0; i < nums.size(); i++){
-            //int cur = target - nums[i];
-            int first = i + 1, last = nums.size() - 1;
-            while(first < last){
-                int cur = nums[first] + nums[last] + nums[i];
-                if(cur == target) return target;
-                if(abs(cur - target) < abs(res - target)) res = cur;
-                if(cur > target) --last;
-                else ++first;
+        //int res;
+        for(int i = 0; i< nums.size(); i++){
+            int low = i + 1, high = nums.size() - 1;
+            while(low < high){
+                int cur = nums[i] + nums[low] + nums[high];
+                if(cur == target) return cur;
+                if(abs(cur - target) < abs(sum - target)) sum = cur;
+                if(cur < target) ++low;
+                else --high;
             }
         }
-        return res;
+        return sum;
     }
 };
