72a4ab3fda4e6b5f49c49e5c173047e41dd7b61e
sun <lossrual@163.com>
2016-07-13 10:44:27 +0800

    [47. Permutations II][Accepted]committed by LeetCode Extension

---
+++ 47-Permutations-II/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size() == 0) return res;
+        sort(nums.begin(), nums.end());
+        res.push_back(nums);
+        while(next_permutation(nums)){
+            res.push_back(nums);
+        }
+        return res;
+    }
+    bool next_permutation(vector<int>& nums){
+        int i = nums.size() - 2;
+        while(i >= 0 && nums[i] >= nums[i + 1]){
+            i--;
+        }
+        int j = i + 1;
+        if(i >= 0){
+            while(j < nums.size() && nums[j] > nums[i]){
+                ++j;
+            }
+            --j;
+            swap(nums[i], nums[j]);
+        }else{
+            return false;
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+        return true;
+    }
+};
