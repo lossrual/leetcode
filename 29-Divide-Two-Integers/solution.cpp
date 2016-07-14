7962d9f89291462c5b4d6766db77a295d130b07d
sun <lossrual@163.com>
2016-07-14 19:24:22 +0800

    [29. Divide Two Integers][Accepted]committed by LeetCode Extension

--- 29-Divide-Two-Integers/solution.cpp
+++ 29-Divide-Two-Integers/solution.cpp
@@ -2,39 +2,24 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         if(divisor == 0) return INT_MAX;
-        if(dividend ==0) return 0;
+        if(dividend == 0 && divisor != 0) return 0;
         if(dividend == INT_MIN && divisor == -1) return INT_MAX;
-        int res = 0;
-        int sign = ((dividend < 0) ^ (divisor < 0))? -1 : 1;
-        long long dvd = labs(dividend); long long dvs = labs(divisor);
+        int sign = ((dividend < 0)^(divisor < 0)) ? -1 : 1;
         int digit = 0;
+        long long dvd = labs(dividend), dvs = labs(divisor);
         while(dvs <= (dvd >> 1)){
-            //std::cout << (dvd >> 1) <<'\n';
             dvs <<= 1;
             ++digit;
         }
-        //std::cout << digit << '\n';
+        int res = 0;
         while(digit >= 0){
             if(dvd >= dvs){
-                res += 1 << digit;
+                res += 1<<digit;
                 dvd -= dvs;
             }
             dvs >>= 1;
-            digit--;
-            std::cout << dvd << ' ' << dvs << ' ' << digit << '\n';
+            --digit;
         }
-        /*
-        while(dvd >= dvs){
-            long long tmp = dvs, multiple = 1;
-            std::cout << "tmp: " << tmp << " " << "multiple: " << multiple << '\n';
-            while(dvd >= (tmp << 1)){
-                tmp <<= 1;
-                multiple <<= 1;
-            }
-            dvd -= tmp;
-            res += multiple;
-        }
-        */
-        return sign == 1? res : -res;
+        return sign < 0? -res : res; 
     }
 };
