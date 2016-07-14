a2a0687b8877584c6f07d396891248af0ad73d53
sun <lossrual@163.com>
2016-07-14 11:41:05 +0800

    [40. Combination Sum II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 40-Combination-Sum-II/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> path;
+        vector<vector<int>> res;
+        helper(candidates, res, path, target, 0);
+        return res;
+    }
+    void helper(vector<int>& candidates, vector<vector<int>>& res, vector<int>& path, int target, int start){
+        if(target == 0){
+            res.push_back(path);
+            return;
+        }
+        //if(target < 0) return;
+        int flag = -1;
+        for(int i = start; i < candidates.size(); i++){
+            if(candidates[i] == flag) continue;
+            path.push_back(candidates[i]);
+            flag = candidates[i];
+            helper(candidates, res, path, target - candidates[i], i + 1);
+            path.pop_back();
+            
+        }
+    }
+};
