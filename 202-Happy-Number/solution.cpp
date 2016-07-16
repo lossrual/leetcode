aa4e2d5acb9586aba7e73a38b1cbd5145ee2941f
sun <lossrual@163.com>
2016-07-16 17:39:01 +0800

    [202. Happy Number][Accepted]committed by LeetCode Extension

---
+++ 202-Happy-Number/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n == 0) return false;
+        int slow = square(n);
+        int fast = square(square(n));
+        while(slow != fast){
+            slow = square(slow);
+            fast = square(square(fast));
+        }
+        return slow == 1;
+    }
+    int square(int n){
+        int sum = 0;
+        while(n){
+            int tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+};
