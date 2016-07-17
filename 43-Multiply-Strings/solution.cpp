7b9bcaef6c76ab79d12a41d5c0de361fa8f880db
sun <lossrual@163.com>
2016-07-17 18:27:13 +0800

    [43. Multiply Strings][Runtime Error]committed by LeetCode Extension

--- 43-Multiply-Strings/solution.cpp
+++ 43-Multiply-Strings/solution.cpp
@@ -3,24 +3,24 @@ public:
     string multiply(string num1, string num2) {
         int m = num1.size(), n = num2.size();
         if(m == 0 && n == 0) return "";
+        string res(m + n, '0');
         reverse(num1.begin(), num1.end());
         reverse(num2.begin(), num2.end());
-        string res(m + n, '0');
-        for(int j = 0; j < n; j++){
-            int sum = 0;
+        for(int j = 0; j < m; j++){
             int val2 = num2[j] - '0';
-            for(int i = 0; i < m; i++){
+            int sum = 0, extra = 0;
+            for(int i = 0; i < n; i++){
                 int val1 = num1[i] - '0';
-                sum += (val2 * val1 + res[i + j] - '0');
+                sum = val1 * val2 + res[i + j] - '0' + extra;
+                //std::cout << sum << std::endl;
                 res[i + j] = sum % 10 + '0';
-                sum /= 10;
+                extra = sum / 10;
             }
-            std::cout << sum << std::endl;
-            if(sum != 0) res[j + m] = sum + '0';
+            if(extra) res[j + m] = extra + '0';
         }
         reverse(res.begin(), res.end());
         int count = 0;
-        while(count < res.size() - 1 && res[count] == '0') ++count;
+        while(count < res.size() - 1 && res[count] == '0') count++;
         res.erase(0, count);
         return res;
     }
