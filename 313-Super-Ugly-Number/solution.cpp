c1732425f353ab55d670d51e91843869c8efa106
sun <lossrual@163.com>
2016-07-17 12:12:37 +0800

    [313. Super Ugly Number][Accepted]committed by LeetCode Extension

--- 313-Super-Ugly-Number/solution.cpp
+++ 313-Super-Ugly-Number/solution.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        vector<int> index(primes.size(), 0), ugly(n, INT_MAX);
-        ugly[0] = 1;
+        vector<int> res = helper(primes, n);
+        return res[n - 1];
+    }
+    vector<int> helper(vector<int>& primes, int n){
+        vector<int> res(n, INT_MAX);
+        res[0] = 1; 
+        int k = primes.size();
+        vector<int> idx(k, 0);
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < primes.size(); j++){
-                ugly[i] = min(ugly[i], ugly[index[j]] * primes[j]);
+            for(int j = 0; j < k; j++){
+                res[i] = min(res[i], res[idx[j]] * primes[j]);
             }
-            for(int j = 0; j < primes.size(); j++){
-                index[j] += (ugly[i] == ugly[index[j]] * primes[j]);
+            for(int j = 0; j < k; j++){
+                idx[j] += (res[i] == res[idx[j]] * primes[j]);
             }
         }
-        return ugly[n - 1];
+        return res;
     }
 };
