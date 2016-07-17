f5f24565756e82f5b57a19299499f6cc4e5531d7
sun <lossrual@163.com>
2016-07-17 14:04:33 +0800

    [204. Count Primes][Time Limit Exceeded]committed by LeetCode Extension

--- 204-Count-Primes/solution.cpp
+++ 204-Count-Primes/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countPrimes(int n) {
         if(n == 0 || n == 1) return 0;
-        if(n == 2) return 0;
+        //if(n == 2) return 0;
         int count = 0;
         for(int i = 2; i < n; i++){
             if(isprime(i))
