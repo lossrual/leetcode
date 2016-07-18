134bf1ff11de2202313f68d3ca1017bd53b9f9c0
sun <lossrual@163.com>
2016-07-18 10:38:31 +0800

    [14. Longest Common Prefix][Accepted]committed by LeetCode Extension

--- 14-Longest-Common-Prefix/solution.cpp
+++ 14-Longest-Common-Prefix/solution.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         //string res = "";
+        if(strs.size() == 0) return "";
         int i;
         for(i = 0; i < strs[0].size(); i++){
             for(int j = 1; j < strs.size(); j++){
