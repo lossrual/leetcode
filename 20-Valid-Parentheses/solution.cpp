925174b7a7815e57bc3f68891ab6c6a7703aba20
sun <lossrual@163.com>
2016-07-18 10:52:38 +0800

    [20. Valid Parentheses][Accepted]committed by LeetCode Extension

---
+++ 20-Valid-Parentheses/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> ss;
+        unordered_map<char, char> mp;
+        mp['('] = ')';
+        mp['['] = ']';
+        mp['{'] = '}';
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+                ss.push(s[i]);
+            else if(ss.empty() || mp[ss.top()] != s[i])
+                return false;
+            else 
+                ss.pop();
+        }
+        return ss.empty();
+    }
+};
