8cc4713b4a7e619d72d4a358a4d7d2b0b6627f4e
sun <lossrual@163.com>
2016-07-18 15:21:52 +0800

    [28. Implement strStr()][Time Limit Exceeded]committed by LeetCode Extension

--- 28-Implement-strStr()/solution.cpp
+++ 28-Implement-strStr()/solution.cpp
@@ -3,8 +3,8 @@ public:
     int strStr(string haystack, string needle) {
         if(needle.size() == 0) return 0;
         for(int i = 0; i < haystack.size(); i++){
-            int k = 0, j = 0;
-            while(k < needle.size() && j < haystack.size() && needle[k] == haystack[j + i]){
+            int k = 0, j = i;
+            while(k < needle.size() && j < haystack.size() && needle[k] == haystack[j]){
                 ++k;
                 ++j;
             }
