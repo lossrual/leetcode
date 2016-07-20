dff04a63ffaa6516d2070b25c23878c5c8c333fe
sun <lossrual@163.com>
2016-07-20 12:36:10 +0800

    [134. Gas Station][Accepted]committed by LeetCode Extension

--- 134-Gas-Station/solution.cpp
+++ 134-Gas-Station/solution.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-//O(n)
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size();
-        int index = 0, total = 0, tank = 0;
-        for(int i = 0; i < n; i++){
-            tank += (gas[i] - cost[i]);
-            total += (gas[i] - cost[i]);
+        int total = 0, tank = 0, index = 0;
+        for(int i = 0; i < gas.size(); i++){
+            int diff = gas[i] - cost[i];
+            tank += diff;
+            total += diff;
             if(tank < 0){
-                index = i + 1;
-                //total += tank;
                 tank = 0;
+                index = i + 1;
             }
         }
         return total < 0 ? -1 : index;
-    }
-    //O(n),O(1)
-    int canCompleteCircuit1(vector<int>& gas, vector<int>& cost) {
-        int start = gas.size() - 1, end = 0;
-        int total = gas[start] - cost[start];
-        while(end < start){
-            if(total >= 0){
-                total += gas[end] - cost[end];
-                ++end;
-            }else{
-                --start;
-                total += gas[start] - cost[start];
-            }
-        }
-        return total < 0 ? -1 : start;
     }
 };
