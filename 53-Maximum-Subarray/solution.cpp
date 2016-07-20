fd271f59c22abb43abdc7e4a9e2393b82f4176ed
sun <lossrual@163.com>
2016-07-20 10:22:17 +0800

    [53. Maximum Subarray][Accepted]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.cpp
+++ 53-Maximum-Subarray/solution.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int res = nums[0];
-        int sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
-            res = max(sum, res);
-            sum = max(sum, 0);
+        int sum = nums[0];
+        int maxn = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            if(sum < 0){
+                sum = nums[i];
+            }else{
+                sum += nums[i];
+            }
+            if(sum > maxn){
+                maxn = sum;
+            }
         }
-        return res;
+        return maxn;
     }
 };
