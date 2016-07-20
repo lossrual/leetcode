961e9b411a87acfda1ae78a847f367f3eeb327d5
sun <lossrual@163.com>
2016-07-20 13:08:19 +0800

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.cpp
+++ 62-Unique-Paths/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
+    int uniquePaths1(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, 0));
         for(int j = 0; j < n; j++)
             dp[0][j] = 1;
@@ -11,5 +11,15 @@ public:
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
         return dp[m - 1][n - 1];
+    }
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 0);
+        for(int j = 0; j < n; j++)
+            dp[j] = 1;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++)
+                dp[j] += dp[j - 1];
+        }
+        return dp[n - 1];
     }
 };
