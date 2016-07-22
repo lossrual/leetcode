b913afc794cd33e8aca7f0625e30089324880497
sun <lossrual@163.com>
2016-07-22 21:31:45 +0800

    [146. LRU Cache][Compile Error]committed by LeetCode Extension

--- 146-LRU-Cache/solution.cpp
+++ 146-LRU-Cache/solution.cpp
@@ -3,6 +3,7 @@ public:
     LRUCache(int capacity) {
         this->capacity_ = capacity;
     }
+    
     int get(int key) {
         if(cacheMap.find(key) == cacheMap.end()){
             return -1;
@@ -15,20 +16,17 @@ public:
     void set(int key, int value) {
         if(cacheMap.find(key) != cacheMap.end()){
             cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-            cacheList.begin()->second = value;
-        }else{
-            if(cacheList.size() == capacity_){
-                int key = cacheList.back().first;
-                cacheMap.erase(key);
-                cacheList.pop_back();
-            }
-            cacheList.push_front(make_pair(key, value));
-            cacheMap[key] = cacheList.begin();
-            std::cout << cacheMap[key] << std::endl;
+            cacheMap[key]->second = value;
+        }else if(capacity_ == cacheList.size()){
+            int key = cacheList.back().first;
+            cacheMap.erase(key);
+            cacheList.pop_back();
         }
+        cacheList.push_front(make_pair(key, value));
+        cacheMap[key] = cacheList.begin();
     }
 private:
     int capacity_;
-    list<pair<int, int>> cacheList;
+    list<int, int> cacheList;
     unordered_map<int, list<pair<int, int>>::iterator> cacheMap;
 };
