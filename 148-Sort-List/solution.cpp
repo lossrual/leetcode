30b058c44ab1d4b6bdd2f7d8fe68a7d879f43254
sun <lossrual@163.com>
2016-09-02 15:02:40 +0800

    [148. Sort List][Time Limit Exceeded]committed by LeetCode Extension

--- 148-Sort-List/solution.cpp
+++ 148-Sort-List/solution.cpp
@@ -8,17 +8,46 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
+    ListNode* sortList1(ListNode* head) {
         if(!head || !head->next) return head;
         ListNode *fast = head, *slow = head;
         while(fast->next && fast->next->next){
             slow = slow->next;
             fast = fast->next->next;
         }
+        //slow是中间结点，slow->next是下一个结点
         ListNode* mid = slow->next;
+        //std::cout << mid->val << std::endl;
         slow->next = nullptr;
         return merge(sortList(head),sortList(mid));
     }
+    ListNode* partition(ListNode* start, ListNode* end){
+        if(start == end) return nullptr;
+        ListNode* p1 = start, *p2 = start->next;
+        int key = p1->val;
+        while(p2 != end){
+            if(key > p2->val){
+                p1 = p1->next;
+                swap(p1->val, p2->val);
+            }
+            p2 = p2->next;
+        }
+        swap(p1->val, start->val);
+        return p1;
+    }
+    void quicksort(ListNode* start, ListNode* end)
+    {
+        if(start != end){
+            ListNode* pt = partition(start, end);
+            quicksort(start, pt);
+            quicksort(pt->next, end);
+        }
+    }
+    ListNode* sortList(ListNode* head) {
+        quicksort(head, nullptr);
+        return head;
+    }
+     
 private:
     ListNode* merge(ListNode* l1, ListNode* l2){
         if(l1 == nullptr) return l2;
