75b92e8909867770394ca57b3381fce811102d4a
sun <lossrual@163.com>
2016-09-07 10:30:24 +0800

    [225. Implement Stack using Queues][Time Limit Exceeded]committed by LeetCode Extension

--- 225-Implement-Stack-using-Queues/solution.cpp
+++ 225-Implement-Stack-using-Queues/solution.cpp
@@ -3,16 +3,19 @@ public:
     // Push element x onto stack.
     queue<int> que1;
     queue<int> que2;
-    int a;
+    //int a;
     void push(int x) {
         que1.push(x);
-        a = x;
+        if(que1.size() == 1) return;
+        int tmp = que1.front();
+        que2.push(tmp);
+        que1.pop();
     }
 
     // Removes the element on top of the stack.
     void pop() {
         while(que1.size() > 1){
-            a = que1.front();
+            int a = que1.front();
             que2.push(a);
         }
         que1.pop();
@@ -24,7 +27,7 @@ public:
 
     // Get the top element.
     int top() {
-        return a;
+        return que1.front();
     }
 
     // Return whether the stack is empty.
