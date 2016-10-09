c360e46cf636fa8560515456186e88d733628773
sun <lossrual@163.com>
2016-10-09 18:53:39 +0800

    [198. House Robber][Runtime Error]committed by LeetCode Extension

---
+++ 198-House-Robber/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int res = 0;
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0]; dp[1] = max(nums[0],nums[1]);
+        for(int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);//状态转移
+        }
+        return dp[nums.size() - 1];
+    }
+};
