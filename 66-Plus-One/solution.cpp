154d47ed58f7a047707ce30f5a046a6f62d935e6
sun <lossrual@163.com>
2016-10-09 19:30:07 +0800

    [66. Plus One][Accepted]committed by LeetCode Extension

--- 66-Plus-One/solution.cpp
+++ 66-Plus-One/solution.cpp
@@ -3,14 +3,13 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         vector<int> res(digits.size(), 0);
         int extra = 1, sum = 0;
-        for(int i = digits.size() - 1; i >= 0; i--){
+        for(int i = digits.size() - 1; i >= 0; i--) {
             sum = digits[i] + extra;
-            extra = sum / 10;
             res[i] = sum % 10;
+            extra = sum / 10;
         }
-        if(extra){
-            res.insert(res.begin(), extra);
-        }
+        if(extra)
+            res.insert(res.begin(), 1);
         return res;
     }
 };
