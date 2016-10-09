50bc3edc1a8914154c2a629ddcace42800affd1f
sun <lossrual@163.com>
2016-10-09 20:19:44 +0800

    [88. Merge Sorted Array][Accepted]committed by LeetCode Extension

--- 88-Merge-Sorted-Array/solution.cpp
+++ 88-Merge-Sorted-Array/solution.cpp
@@ -3,14 +3,14 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int a = m - 1, b = n - 1;
         int c = m + n - 1;
-        while(a >= 0  && b >= 0){
-            if(nums1[a] > nums2[b]){
+        while(a >= 0 && b >= 0) {
+            if(nums1[a] > nums2[b])
                 nums1[c--] = nums1[a--];
-            }else{
+            else
                 nums1[c--] = nums2[b--];
-            }
         }
-        while(b >= 0)
+        while(b >= 0) {
             nums1[c--] = nums2[b--];
+        }
     }
 };
