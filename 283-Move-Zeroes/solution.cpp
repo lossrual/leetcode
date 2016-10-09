771982dfb55db576e6a3915c87aad90d57b1a651
sun <lossrual@163.com>
2016-10-09 16:57:11 +0800

    [283. Move Zeroes][Accepted]committed by LeetCode Extension

--- 283-Move-Zeroes/solution.cpp
+++ 283-Move-Zeroes/solution.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int i = 0;
-        if(nums.size() == 0) return;
-        while(i < nums.size()){
-            int j = 0;
-            if(nums[i] == 0){
-                for(j = i + 1; j < nums.size(); j++){
-                    if(nums[j] != 0){
-                        swap(nums[i], nums[j]);
+        for(; i < nums.size(); i++) {
+            if(nums[i] == 0) {
+                for(int j = i + 1; j < nums.size(); j++) {
+                    if(nums[j] != 0) {
+                        swap(nums[j], nums[i]);
                         break;
                     }
                 }
             }
-            i++;
         }
     }
 };
