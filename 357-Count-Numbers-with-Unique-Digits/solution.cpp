3854230bfbe2214aa1ac1857fb406dc95d5c460b
sun <lossrual@163.com>
2016-10-09 15:41:22 +0800

    [357. Count Numbers with Unique Digits][Accepted]committed by LeetCode Extension

--- 357-Count-Numbers-with-Unique-Digits/solution.cpp
+++ 357-Count-Numbers-with-Unique-Digits/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-//f(k) = f(k - 1) + 9*9*8*7*...
     int countNumbersWithUniqueDigits(int n) {
-        int res = 10;
-        int uniquenum = 9;
-        int availablenum = 9;
+        int res = 10, tmp = 9, unique = 9;
         if(n == 0) return 1;
-        while(n-- > 1 && availablenum > 0){
-            uniquenum = uniquenum * availablenum;
-            res += uniquenum;
-            availablenum--;
+        if(n == 1) return res;
+        while(n-- > 1 && tmp){
+            unique *= tmp;
+            res += unique;
+            tmp--;
         }
         return res;
     }
