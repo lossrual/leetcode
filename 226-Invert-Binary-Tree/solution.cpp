62f5f40824c4266e5e0ee3bf9e057488cb1253cf
sun <lossrual@163.com>
2016-10-09 18:45:25 +0800

    [226. Invert Binary Tree][Accepted]committed by LeetCode Extension

--- 226-Invert-Binary-Tree/solution.cpp
+++ 226-Invert-Binary-Tree/solution.cpp
@@ -9,30 +9,11 @@
  */
 class Solution {
 public:
-    TreeNode* invertTree1(TreeNode* root) {
+    TreeNode* invertTree(TreeNode* root) {
         if(!root) return root;
-        if(root){
-            root->left = invertTree(root->left);
-            root->right = invertTree(root->right);
-            swap(root->left, root->right);
-        }
-        return root;
-    }
-    TreeNode* invertTree(TreeNode* root){
-        if(!root) return root;
-        queue<TreeNode*> q;
-        TreeNode* cur = root;
-        q.push(root);
-        while(!q.empty()){
-            int len = q.size();
-            for(int i = 0; i < len; i++){
-                auto node = q.front();
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-                swap(node->left, node->right);
-                q.pop();
-            }
-        }
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        swap(root->left, root->right);
         return root;
     }
 };
