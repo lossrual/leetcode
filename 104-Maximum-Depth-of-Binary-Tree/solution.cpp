f544c9da774014b995b120aa7d4db824da5ce652
sun <lossrual@163.com>
2016-10-09 20:50:18 +0800

    [104. Maximum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 104-Maximum-Depth-of-Binary-Tree/solution.cpp
+++ 104-Maximum-Depth-of-Binary-Tree/solution.cpp
@@ -9,13 +9,12 @@
  */
 class Solution {
 public:
+//树的最大深度
     int maxDepth(TreeNode* root) {
-        return depth(root);
-    }
-    int depth(TreeNode* root){
         if(!root) return 0;
-        if(!root->left) return depth(root->right) + 1;
-        if(!root->right) return depth(root->left) + 1;
-        return max(depth(root->left), depth(root->right)) + 1;
+        //int left = 0, right = 0;
+        //if(!root->left) return maxDepth(root->right) + 1;
+        //if(!root->right) return maxDepth(root->left) + 1;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
