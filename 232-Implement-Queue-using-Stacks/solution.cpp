5adc6e7af8f1ddadb90cb46a5c06fc5cc38524fb
sun <lossrual@163.com>
2016-10-09 18:43:10 +0800

    [232. Implement Queue using Stacks][Accepted]committed by LeetCode Extension

--- 232-Implement-Queue-using-Stacks/solution.cpp
+++ 232-Implement-Queue-using-Stacks/solution.cpp
@@ -1,15 +1,14 @@
 class Queue {
 public:
+    stack<int> s1, s2;
     // Push element x to the back of queue.
-    stack<int> s1;
-    stack<int> s2;
     void push(int x) {
-        while(!s1.empty()){
+        while(!s1.empty()) {
             s2.push(s1.top());
             s1.pop();
         }
         s1.push(x);
-        while(!s2.empty()){
+        while(!s2.empty()) {
             s1.push(s2.top());
             s2.pop();
         }
