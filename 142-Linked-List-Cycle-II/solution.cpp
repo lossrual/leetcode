9359a10c784564cba3030eb5b366f75296d13b5d
sun <lossrual@163.com>
2016-10-09 19:06:43 +0800

    [142. Linked List Cycle II][Accepted]committed by LeetCode Extension

--- 142-Linked-List-Cycle-II/solution.cpp
+++ 142-Linked-List-Cycle-II/solution.cpp
@@ -8,32 +8,16 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle1(ListNode *head) {
-        if(!head) return head;
-        ListNode* slow = head, *fast = head;
-        while(fast->next && fast->next->next){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast){
-                ListNode* slow2 = head;
-                while(slow2 != slow){
-                    slow = slow->next;
-                    slow2 = slow2->next;
-                }
-                return slow2;
-            }
-        }
-        return nullptr;
-    }
+    //链表有环,求环的入口
     ListNode *detectCycle(ListNode *head) {
         if(!head) return head;
         ListNode* slow = head, *fast = head;
         while(fast&& fast->next){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast){
-                ListNode* slow2 = head;
-                while(slow2 != slow){
+            if(slow == fast) {
+                ListNode* slow2= head;
+                while(slow != slow2) {
                     slow = slow->next;
                     slow2 = slow2->next;
                 }
