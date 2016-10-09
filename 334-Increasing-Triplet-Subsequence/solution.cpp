f0c074c5ec54ef3cae97613371be065bbb11a60f
sun <lossrual@163.com>
2016-10-09 15:03:57 +0800

    [334. Increasing Triplet Subsequence][Time Limit Exceeded]committed by LeetCode Extension

--- 334-Increasing-Triplet-Subsequence/solution.cpp
+++ 334-Increasing-Triplet-Subsequence/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) {
+    bool increasingTriplet1(vector<int>& nums) {
         int c1 = INT_MAX, c2 = INT_MAX;
         for(auto& x: nums){
             if(x <= c1){
@@ -12,5 +12,22 @@ public:
             }
         }
         return false;
+    }
+    bool increasingTriplet(vector<int>& nums) {
+        return helper(nums) >= 3;
+    }
+    int helper(vector<int>& nums) {
+        int dp[100000];
+        int maxp = 0;
+        dp[0] = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+                 maxp = max(maxp, dp[i]);
+            }
+        }
+        return maxp;
     }
 };
