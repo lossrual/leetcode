e8776b344f4a6f523498524929e64547c9850f80
sun <lossrual@163.com>
2016-10-11 20:30:04 +0800

    [50. Pow(x, n)][Time Limit Exceeded]committed by LeetCode Extension

--- 50-Pow(x,-n)/solution.cpp
+++ 50-Pow(x,-n)/solution.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(x == 0) return 0;
-        if(x == 1 || n == 0) return 1;
-        if(x == -1){
-            return (n % 2 == 0) ? 1 : -1;
-        }
-        if(n < 0){
+        if(x == 1) return 1;
+        if(x == -1) return n % 2 == 0? 1 : -1;
+        if(n < 0) {
             x = 1 / x;
             n = -n;
         }
-        if(n == INT_MIN) return 0;
-        return quick_pow(x, n);
+        return quickPow(x, n);
     }
-    double quick_pow(double x, int n){
+    double quickPow(double x, int n) {
         double res = 1;
-        while(n){
-            if(n & 0x1) res = res * x;
-            x = x * x;
+        while(n) {
+            if(n & 1)
+                res *= x;
+            x *= x;
             n >>= 1;
         }
         return res;
