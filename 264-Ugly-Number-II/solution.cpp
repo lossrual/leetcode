38c48ad3819c215b436d9905504a47704d234394
sun <lossrual@163.com>
2016-10-11 16:37:14 +0800

    [264. Ugly Number II][Accepted]committed by LeetCode Extension

--- 264-Ugly-Number-II/solution.cpp
+++ 264-Ugly-Number-II/solution.cpp
@@ -3,12 +3,12 @@ public:
     int nthUglyNumber(int n) {
         vector<int> res(1, 1);
         int i = 0, j = 0, k = 0;
-        while(res.size() < n){
+        while(--n) {
             res.push_back(min(min(res[i] * 2, res[j] * 3), res[k] * 5));
             if(res.back() == res[i] * 2) ++i;
             if(res.back() == res[j] * 3) ++j;
             if(res.back() == res[k] * 5) ++k;
-        }
-        return res[n - 1];
+        } 
+        return res.back();
     }
 };
