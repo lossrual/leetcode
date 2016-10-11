7f2cbb365a0d39963cb7331f71fc3bbe2b51c115
sun <lossrual@163.com>
2016-10-11 20:22:03 +0800

    [55. Jump Game][Accepted]committed by LeetCode Extension

--- 55-Jump-Game/solution.cpp
+++ 55-Jump-Game/solution.cpp
@@ -3,8 +3,8 @@ public:
     bool canJump(vector<int>& nums) {
         if(nums.size() == 0) return false;
         int reach = 0;
-        for(int i = 0; i < nums.size() && i <= reach; i++){
-            reach = max(nums[i] + i, reach);
+        for(int i = 0; i < nums.size() && i <= reach; i++) {
+            reach = max(reach, i + nums[i]);
         }
         return reach >= nums.size() - 1;
     }
