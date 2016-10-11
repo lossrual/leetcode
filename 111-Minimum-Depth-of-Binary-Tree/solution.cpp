e0894b24600bb937fd52c049ff198f91aa334f26
sun <lossrual@163.com>
2016-10-11 14:36:13 +0800

    [111. Minimum Depth of Binary Tree][Runtime Error]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.cpp
+++ 111-Minimum-Depth-of-Binary-Tree/solution.cpp
@@ -10,13 +10,6 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        return depth(root);
-    }
-private:
-    int depth(TreeNode* root){
-        if(!root) return 0;
-        if(!root->left) return depth(root->right) + 1;
-        if(!root->right) return depth(root->left) + 1;
-         return min(depth(root->right), depth(root->left)) + 1;
+        return min(minDepth(root->left), minDepth(root->right)) + 1;
     }
 };
