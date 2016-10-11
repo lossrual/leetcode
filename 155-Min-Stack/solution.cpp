bee4a5b194a0ebee23863e4cc93386bee42ccd73
sun <lossrual@163.com>
2016-10-11 14:20:31 +0800

    [155. Min Stack][Accepted]committed by LeetCode Extension

--- 155-Min-Stack/solution.cpp
+++ 155-Min-Stack/solution.cpp
@@ -1,57 +1,33 @@
-//class MinStack {
-//public:
-//    /** initialize your data structure here. */
-/*    stack<int> s1;
-    stack<int> s2;
-    MinStack() {
-        
-    }
-    
-    void push(int x) {
-        s1.push(x);
-        if(s2.empty() || x <= getMin()) s2.push(x);
-    }
-    
-    void pop() {
-        if(s1.top() <= getMin()) s2.pop();
-        s1.pop();
-    }
-    
-    int top() {
-        return s1.top();
-    }
-    
-    int getMin() {
-        return s2.top();
-    }
-};
-*/
-
-//实现一个最小栈,push,pop,top,min都是O(1)
 class MinStack {
 public:
     /** initialize your data structure here. */
-    stack<pair<int, int>> s1;
-    MinStack() {}
+    stack<pair<int, int>> s;
+    MinStack() {
+    }
+    
     void push(int x) {
         int minp;
-        if(s1.empty()) {
+        if(s.empty()) {
             minp = x;
         }else {
-            minp = min(s1.top().second, x);
+            minp = min(x, s.top().second);
         }
-        s1.push({x, minp});
+        s.push(make_pair(x, minp));
     }
+    
     void pop() {
-        s1.pop();
+        s.pop();
     }
+    
     int top() {
-        return s1.top().first;
+        return s.top().first;
     }
+    
     int getMin() {
-        return s1.top().second;
+        return s.top().second;
     }
 };
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
