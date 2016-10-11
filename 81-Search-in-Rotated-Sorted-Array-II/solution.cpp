a16ad9a47975707bd70e6dc29d99f4e4b7ca5b46
sun <lossrual@163.com>
2016-10-11 16:12:05 +0800

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

--- 81-Search-in-Rotated-Sorted-Array-II/solution.cpp
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int low = 0, high = nums.size() - 1;
-        while(low <= high){
-            int mid = (low + high) / 2;
+        while(low <= high) {//这里的等号尤其需要注意
+            int mid = low + (high - low) / 2;
             if(nums[mid] == target) return true;
-            if(nums[low] < nums[mid]){
-                if(nums[low] <= target && target < nums[mid]){
+            if(nums[low] < nums[mid]) {
+                if(nums[mid] > target && target >= nums[low])
                     high = mid;
-                }else{
+                else
                     low = mid + 1;
-                }
-            }else if(nums[low] > nums[mid]){
-                if(nums[mid] < target && target <= nums[high]){
+            }else if(nums[low] > nums[mid]) {
+                if(nums[high] >= target && target > nums[mid])
                     low = mid + 1;
-                }else{
+                else 
                     high = mid;
-                }
             }else{
-                ++low;
+                low++;
             }
         }
         return false;
