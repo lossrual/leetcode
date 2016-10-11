6a12385cc99e3d3b77fb7e65c2888e2e65611998
sun <lossrual@163.com>
2016-10-11 15:57:35 +0800

    [64. Minimum Path Sum][Accepted]committed by LeetCode Extension

--- 64-Minimum-Path-Sum/solution.cpp
+++ 64-Minimum-Path-Sum/solution.cpp
@@ -8,11 +8,9 @@ public:
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         for(int j = 1; j < n; j++)
             dp[0][j] = dp[0][j - 1] + grid[0][j];
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
+        for(int i = 1; i < m; i++)
+            for(int j = 1; j < n; j++)
                 dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-            }
-        }
         return dp[m - 1][n - 1];
     }
 };
