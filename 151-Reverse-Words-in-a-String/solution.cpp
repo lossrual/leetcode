3289b94631c550f82f663a12d65b2dd94d439dd3
sun <lossrual@163.com>
2016-10-12 19:43:30 +0800

    [151. Reverse Words in a String][Compile Error]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.cpp
+++ 151-Reverse-Words-in-a-String/solution.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-/*
     void reverseWords(string &s) {
-        if(s == "") return;
-        stack<char> ss;
-        reverse(s.begin(), s.end());
-        for(int i = 0; i < s.size(); i++){
-            ss.push(s[i]);
-        }
-        s.clear();
-        while(!ss.empty()){
-            auto cur = ss.top();
-            ss.pop();
-            s += cur; 
-        }
-    }
-*/
-    void reverseWords(string& s){
         string res = "";
-        for(int i = 0; i < s.size(); i++){
+        if(s.size() == 0) return res;
+        for(int i = 0; i < s.size(); i++) {
             int pos = i;
             if(s[i] == ' ') continue;
             while(s[i] != ' ' && i < s.size()) ++i;
-            if(res.size() > 0) res = ' ' + res;
+            if(res.size()) res = " " + res;
             res = s.substr(pos, i - pos) + res;
         }
-        s = res;
+        res = s;
     }
 };
