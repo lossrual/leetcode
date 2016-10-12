c8d7929c8807ff69c12cde769be8030b6689e912
sun <lossrual@163.com>
2016-10-12 10:44:40 +0800

    [148. Sort List][Accepted]committed by LeetCode Extension

--- 148-Sort-List/solution.cpp
+++ 148-Sort-List/solution.cpp
@@ -8,25 +8,35 @@
  */
 class Solution {
 public:
+//链表归并排序
     ListNode* sortList1(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode *fast = head, *slow = head;
-        while(fast->next && fast->next->next){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        //slow是中间结点，slow->next是下一个结点
-        ListNode* mid = slow->next;
-        //std::cout << mid->val << std::endl;
-        slow->next = nullptr;
-        return merge(sortList(head),sortList(mid));
+       if(!head || !head->next) return head;
+       ListNode* slow = head, *fast = head;
+       while(fast->next && fast->next->next) {
+           slow = slow->next;
+           fast = fast->next->next;
+       }
+       ListNode* mid = slow->next;
+       slow->next = nullptr;
+       return merge(sortList(head), sortList(mid));
     }
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        if(l1->val <= l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        }else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
+    //链表快排
     ListNode* partition(ListNode* start, ListNode* end){
-        if(start == end) return nullptr;
         ListNode* p1 = start, *p2 = start->next;
-        int key = p1->val;
-        while(p2 != end){
-            if(key > p2->val){
+        int pivot = p1->val;
+        while(p2 != end) {
+            if(p2->val < pivot){
                 p1 = p1->next;
                 swap(p1->val, p2->val);
             }
@@ -35,7 +45,7 @@ public:
         swap(p1->val, start->val);
         return p1;
     }
-    void quicksort1(ListNode* start, ListNode* end)
+    void quicksort(ListNode* start, ListNode* end)
     {
         if(start != end){
             ListNode* pt = partition(start, end);
@@ -43,30 +53,8 @@ public:
             quicksort(pt->next, end);
         }
     }
-    void quicksort(ListNode* start, ListNode* end)
-    {
-        while(start != end){
-            ListNode* pt = partition(start, end);
-            quicksort(start, pt);
-            start = pt->next;
-            //quicksort(pt->next, end);
-        }
-    }
     ListNode* sortList(ListNode* head) {
         quicksort(head, nullptr);
         return head;
-    }
-     
-private:
-    ListNode* merge(ListNode* l1, ListNode* l2){
-        if(l1 == nullptr) return l2;
-        if(l2 == nullptr) return l1;
-        if(l1->val <= l2->val){
-            l1->next = merge(l1->next, l2);
-            return l1;
-        }else{
-            l2->next = merge(l1, l2->next);
-            return l2;
-        }
     }
 };
