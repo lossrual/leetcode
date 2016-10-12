d1f3d4160755c56a82326be69b74e2733a4ba160
sun <lossrual@163.com>
2016-10-12 20:10:28 +0800

    [152. Maximum Product Subarray][Accepted]committed by LeetCode Extension

--- 152-Maximum-Product-Subarray/solution.cpp
+++ 152-Maximum-Product-Subarray/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int minp = nums[0];
-        int maxp = nums[0];
-        int maxn = nums[0];
-        //int start = 0, end = 0, curstart = 0;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] >= 0){
+        int maxp = nums[0], minp = nums[0], res = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] >= 0) {
                 maxp = max(maxp * nums[i], nums[i]);
                 minp = min(minp * nums[i], nums[i]);
-            }else{
+            }else {
                 int tmp = maxp;
                 maxp = max(minp * nums[i], nums[i]);
                 minp = min(tmp * nums[i], nums[i]);
             }
-            maxn = max(maxn, maxp);
+            res = max(maxp, res);
         }
-        return maxn;
+        return res;
     }
 };
